e3003b7b2483e6bcf52fe9d73455b3d1f701654c
swish24 <45886223+swish24@users.noreply.github.com>
2018-12-21 15:18:35 -0600

    Did a lot of jscripting

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// JavaScript Document
+var car = {
+			make: 'Chevy',
+			type: 'Silverado',
+			color: 'Maroon',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: [
+			'seat 1',
+			'seat 2',
+			'seat 3',
+			'seat 4',
+			],
+	
+			turnOn: function () {
+				this.isTurnedOn = true;
+			},
+			turnOff: function () {
+				this.isTurnedOn = false;
+			},
+			fly: function () {
+				alert('fly');
+			},
+			switchCar: function (isON){
+				console.log('turn car '+isON)
+				if (isON == true) {
+					this.isTurnedOn = true;
+				} else {
+					this.isTurnedOn = false;
+				}
+			}
+		};
+
+		
+		
+		console.log('hello there friends!')
+
+var sayMyName = function (name) {
+	alert('My name is: '+name)
+}
